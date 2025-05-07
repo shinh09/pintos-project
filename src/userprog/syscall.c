d9526f2c0679ed786029ee6cd85e81735bd632bd
Baikseungwoo <swbaik01@naver.com>
2025-05-07 05:32:00 +0000

    2.2 syscall handler

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -13,8 +13,47 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+  void *esp;
+  int syscall_number;
+
+  esp = f->esp;
+
+  /* 1. Validate stack pointer */
+  if (!is_valid_ptr(esp)) {
+  exit(-1);  /* or thread_exit(), depending on your policy */
+  }
+
+  /* 2. Extract system call number */
+  syscall_number = *(int *)esp;
+
+  /* 3. Process system call */
+  switch (syscall_number) {
+  case SYS_HALT:
+    halt();
+    break;
+  case SYS_EXIT:
+    if (!is_valid_ptr(esp + 4)) {
+      exit(-1);
+    }
+    exit(*(int *)(esp + 4));
+    break;
+  case SYS_EXEC:
+    if (!is_valid_ptr(esp + 4)) {
+      exit(-1);
+    }
+    f->eax = exec(*(char **)(esp + 4));
+    break;
+  case SYS_WAIT:
+    if (!is_valid_ptr(esp + 4)) {
+      exit(-1);
+    }
+    f->eax = wait(*(pid_t *)(esp + 4));
+    break;
+  /* Add other cases here */
+  default:
+    exit(-1);
+    break;
+  }
 }
