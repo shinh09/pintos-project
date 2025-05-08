c9a20be5e9020f9a14ff4ecf4ecf73ae5a2f3eb6
mmuhunn <dnwn7359@naver.com>
2025-05-08 11:47:17 +0000

    page_fault complete

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -4,12 +4,18 @@
 #include "userprog/gdt.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
+#include "userprog/pagedir.h"
+
+
 
 /* Number of page faults processed. */
 static long long page_fault_cnt;
 
 static void kill (struct intr_frame *);
 static void page_fault (struct intr_frame *);
+void exit(int status);
+
 
 /* Registers handlers for interrupts that can be caused by user
    programs.
@@ -147,6 +153,13 @@ page_fault (struct intr_frame *f)
   not_present = (f->error_code & PF_P) == 0;
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
+
+  if (user) {
+   if ((void *)fault_addr >= PHYS_BASE ||
+       pagedir_get_page(thread_current()->pagedir, fault_addr) == NULL) {
+     exit(-1);
+   }
+ }
 
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
